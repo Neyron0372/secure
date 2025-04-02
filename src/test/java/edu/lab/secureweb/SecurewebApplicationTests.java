d1a48136619aee0fbbf265f4e08a3e6fa3e6fd7d
Neyron0372 <neyron.kolibri@gmail.com>
2025-04-02 22:06:46 +0300

    initial

---
+++ src/test/java/edu/lab/secureweb/SecurewebApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lab.secureweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurewebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
